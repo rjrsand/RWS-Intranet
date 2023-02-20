2fd3336dbdb5d5882469776aa625aec129c81a29
rjrsand <99208059+rjrsand@users.noreply.github.com>
2023-02-20 16:26:27 -0500

    Added additional styling to index.php

--- index.php
+++ index.php
@@ -13,73 +13,74 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="bootstrap/5/css/bootstrap.min.css" />
     <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="css/stylesheet.css" />
     <link rel="stylesheet" href="css/intranet.css" />
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="dmxAppConnect/dmxBootstrap5Navigation/dmxBootstrap5Navigation.js" defer></script>
 
 </head>
 
-<body is="dmx-app" id="index">
+<body is="dmx-app" id="index" class="body-bg">
     <header>
         <nav class="navbar navbar-expand-lg navbar-light bg-light"></nav>
     </header>
 
 
 
-    <main>
+    <main class="mt-5">
         <div class="container">
             <div class="row">
                 <div class="col">
-                    <h5>Operations Team</h5>
-                    <h1 class="text-left">Welcome to Raging Wolf Solutions</h1>
+                    <h5 class="text-warning text-uppercase">Operations Team</h5>
+                    <h1 class="text-left fw-bold text-light">Welcome to Raging Wolf Solutions</h1>
                     <p class="mb-4 text-secondary" style="max-width: 770px !important">Here you can find helpful tools and resources that are commonly used in your department at Raging Wolf Solutions. If you need any additional information or resources, please contact your manager.</p>
                 </div>
             </div>
             <div class="row">
                 <div class="col-lg-6 align-self-center">
-                    <div class="d-flex pt-3 pb-3 ps-2 pe-2">
+                    <div class="d-flex tile-category mb-3 pt-3 pb-3 ps-2 pe-2">
                         <div class="d-flex align-items-center w-100p justify-content-center">
-                            <h1 class="mb-0"><i class="fas fa-link"></i></h1>
+                            <h1 class="mb-0 text-white-50"><i class="fas fa-link"></i></h1>
                         </div>
-                        <div class="d-flex flex-column">
-                            <h3>Training Resource Center</h3>
-                            <p class="mb-0">Use this page to learn more about commonly used tools.</p>
+                        <div class="d-flex flex-column w-100">
+                            <h3 class="text-light">Training Resource Center</h3>
+                            <p class="mb-0 text-secondary">Use this page to learn more about commonly used tools.</p>
                         </div>
                     </div>
 
                 </div>
                 <div class="col-lg-6">
-                    <div class="d-flex pt-3 pb-3 ps-2 pe-2">
+                    <div class="d-flex tile-category mb-3 pt-3 pb-3 ps-2 pe-2">
                         <div class="d-flex align-items-center w-100p justify-content-center">
-                            <h1 class="mb-0"><i class="fas fa-link"></i></h1>
+                            <h1 class="mb-0 text-white-50"><i class="far fa-file-alt"></i></h1>
                         </div>
-                        <div class="d-flex flex-column">
-                            <h3>Training Resource Center</h3>
-                            <p class="mb-0">Use this page to learn more about commonly used tools.</p>
+                        <div class="d-flex flex-column w-100">
+                            <h3 class="text-light">Document Center</h3>
+                            <p class="mb-0 text-secondary">Use this page to access RWS employee documents.</p>
                         </div>
                     </div>
                 </div>
             </div>
             <div class="row">
                 <div class="col-lg-6">
-                    <div class="d-flex pt-3 pb-3 ps-2 pe-2">
-                        <div class="d-flex align-items-center w-100p justify-content-center">
-                            <h1 class="mb-0"><i class="fas fa-link"></i></h1>
+                    <div class="d-flex tile-category mb-3 pt-3 pb-3 ps-2 pe-2">
+                        <div class="d-flex align-items-center w-100p justify-content-center h-tile-default">
+                            <h1 class="mb-0 text-white-50"><i class="fas fa-phone-alt"></i></h1>
                         </div>
-                        <div class="d-flex flex-column">
-                            <h3>Training Resource Center</h3>
-                            <p class="mb-0">Use this page to learn more about commonly used tools.</p>
+                        <div class="d-flex flex-column w-100">
+                            <h3 class="text-light">Directory</h3>
+                            <p class="mb-0 text-secondary">Use this page to access RWS directory.</p>
                         </div>
                     </div>
                 </div>
                 <div class="col-lg-6">
-                    <div class="d-flex pt-3 pb-3 ps-2 pe-2">
-                        <div class="d-flex align-items-center w-100p justify-content-center">
-                            <h1 class="mb-0"><i class="fas fa-link"></i></h1>
+                    <div class="d-flex tile-category mb-3 pt-3 pb-3 ps-2 pe-2">
+                        <div class="d-flex align-items-center w-100p justify-content-center h-tile-default">
+                            <h1 class="mb-0 text-white-50"><i class="fas fa-toolbox"></i></h1>
                         </div>
-                        <div class="d-flex flex-column">
-                            <h3>Training Resource Center</h3>
-                            <p class="mb-0">Use this page to learn more about commonly used tools.</p>
+                        <div class="d-flex flex-column w-100">
+                            <h3 class="text-light">Commonly Used Tools</h3>
+                            <p class="mb-0 text-secondary">Access this page to use RWS day-to-day tools.</p>
                         </div>
                     </div>
                 </div>
