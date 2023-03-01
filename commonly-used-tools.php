fb6558e13e1b76bd2a013a0ebdaaefd1edfba016
rjrsand <99208059+rjrsand@users.noreply.github.com>
2023-02-28 19:51:31 -0500

    Added third modal

--- commonly-used-tools.php
+++ commonly-used-tools.php
@@ -78,7 +78,7 @@
                         <div class="container-fluid gx-0" is="dmx-form-repeat" id="formRepeat3" dmx-bind:items="1">
                             <div class="row text-nowrap">
                                 <div class="col-auto">
-                                    <p class="h-100">Username: RWS Email</p>
+                                    <p class="h-100">Username: ops@ragingwolfsolutions.com</p>
                                 </div>
                                 <div class="col"><button class="btn btn-outline-link text-warning pt-0 pb-0" data-bs-toggle="button">
 
@@ -90,7 +90,47 @@
                                 <div class="col-auto">
                                     <p class="h-100 ps-0 pe-4">Password: ********</p>
                                 </div>
+                                <div class="col"><button class="btn btn-outline-link text-warning pt-0 pb-0" data-bs-toggle="button" dmx-on:click.capture="********">
+
+                                        <i class="fas fa-copy fa-fw"></i><span class="badge bg-warning text-dark rounded-pill ms-2">Copy</span></button></div>
+                            </div>
+                        </div>
+
+
+                    </div>
+
+                </div>
+                <div class="modal-footer text-warning bg-dark border-0 justify-content-center">
+                    <button type="button" class="btn btn-outline-light" data-bs-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="modal fw-bolder" id="modal3" is="dmx-bs5-modal" tabindex="-1">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header bg-dark text-warning border-bottom border-secondary border-2">
+                    <h5 class="modal-title text-light">Site Credentials</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body bg-dark text-warning">
+                    <div class="flex-container">
+                        <div class="container-fluid gx-0" is="dmx-form-repeat" id="formRepeat4" dmx-bind:items="1">
+                            <div class="row text-nowrap">
+                                <div class="col-auto">
+                                    <p class="h-100">Username: ops@ragingwolfsolutions.com</p>
+                                </div>
                                 <div class="col"><button class="btn btn-outline-link text-warning pt-0 pb-0" data-bs-toggle="button">
+
+                                        <i class="fas fa-copy fa-fw"></i><span class="badge bg-warning text-dark rounded-pill ms-2">Copy</span></button></div>
+                            </div>
+                        </div>
+                        <div class="container-fluid gx-0" is="dmx-form-repeat" id="formRepeat4" dmx-bind:items="1">
+                            <div class="row text-nowrap">
+                                <div class="col-auto">
+                                    <p class="h-100 ps-0 pe-4">Password: ********</p>
+                                </div>
+                                <div class="col"><button class="btn btn-outline-link text-warning pt-0 pb-0" data-bs-toggle="button" dmx-on:click.capture="********">
 
                                         <i class="fas fa-copy fa-fw"></i><span class="badge bg-warning text-dark rounded-pill ms-2">Copy</span></button></div>
                             </div>
@@ -126,14 +166,14 @@
                         </div>
                         <div class="row">
                             <div class="col-sm-6 col-lg-4 text-white-50">
-                                <div class="d-flex tile-category h-tile-default text-start text-white-50 flex-column mb-3 pt-3 pb-3 ps-3 pe-3">
-                                    <div class="d-flex w-50"><img src="assets/images/Intranet/carrier411-logo-gray.png" class="img-fluid mb-1">
+                                <div class="tile-category h-tile-default text-start text-white-50 flex-column mb-3 pt-3 pb-3 ps-3 pe-3">
+                                    <div class="d-flex cut-tool-logo mb-2"><img src="assets/images/Intranet/carrier411-logo-gray.png" class="img-fluid">
                                     </div>
 
-                                    <h6 class="style10 text-secondary mb-3 text-start">
-                                        <font color="#6c757d"><span style="font-size: 14px;">Find and qualify registered freight carriers.</span></font>
+                                    <h6 class="style10 text-secondary text-start mb-3">
+                                        <font color="#6c757d"><span style="font-size: 14px;">Find &amp; qualify registered freight carriers.</span></font>
                                     </h6>
-                                    <div class="d-flex">
+                                    <div class="d-flex pt-2">
                                         <div class="container-fluid gx-0">
                                             <div class="row gx-3">
                                                 <div class="col-lg-6">
@@ -155,11 +195,11 @@
                                 </div>
                             </div>
                             <div class="col-sm-6 col-lg-4 text-white-50">
-                                <div class="d-flex tile-category h-tile-default text-start text-white-50 flex-column pt-2 pb-3 ps-3 pe-3">
-                                    <div class="d-flex w-50"><img src="assets/images/Intranet/Salesforce_Logo.png" class="w-50 img-fluid">
+                                <div class="tile-category h-tile-default text-start text-white-50 flex-column mb-3 pt-3 pb-3 ps-3 pe-3">
+                                    <div class="d-flex cut-tool-logo mb-2"><img src="assets/images/Intranet/Salesforce_Logo.png" class="img-fluid">
                                     </div>
 
-                                    <h6 class="style10 text-secondary text-start mb-2 pt-1">
+                                    <h6 class="style10 text-secondary text-start mb-3">
                                         <font color="#6c757d"><span style="font-size: 14px;">Find all information about all accounts.</span></font>
                                     </h6>
                                     <div class="d-flex pt-2">
@@ -167,6 +207,35 @@
                                             <div class="row gx-3">
                                                 <div class="col-lg-6">
                                                     <a href="https://staff.my.salesforce.com/" target="_blank">
+                                                        <button class="btn btn-outline-secondary w-100 btn-sm text-nowrap mb-2" type="button">
+                                                            <i class="fas fa-share-square"></i>&nbsp;Open
+                                                        </button>
+                                                    </a>
+                                                </div>
+                                                <div class="col-lg-6">
+
+                                                    <button class="btn btn-outline-secondary w-100 btn-sm text-nowrap mb-2" type="button" data-bs-toggle="modal" data-bs-target="#modal2"><i class="fas fa-eye"></i>&nbsp;View Details</button>
+
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                            <div class="col-sm-6 col-lg-4 text-white-50">
+                                <div class="tile-category h-tile-default text-start text-white-50 flex-column mb-3 pt-3 pb-3 ps-3 pe-3">
+                                    <div class="d-flex cut-tool-logo mb-2"><img src="assets/images/Intranet/DAT_Solutions_Logo.png" class="img-fluid">
+                                    </div>
+
+                                    <h6 class="style10 text-secondary text-start mb-3">
+                                        <font color="#6c757d"><span style="font-size: 14px;">Find unqualified freight carriers.</span></font>
+                                    </h6>
+                                    <div class="d-flex pt-2">
+                                        <div class="container-fluid gx-0">
+                                            <div class="row gx-3">
+                                                <div class="col-lg-6">
+                                                    <a href="https://login.dat.com/login?state=hKFo2SBETnJlYkw1ektrcTQxbWdNMjJfeWU1dGpER3Zkbkl0c6FupWxvZ2luo3RpZNkgSENXelNibHJWS0xDRmJqc3JrdVl5VEkwb0ZoSHUxUTWjY2lk2SBQcHl2b1JHMk5CcGdiQlZmWGpsMUdhZ1RuaTRSWkh4UA&amp;client=PpyvoRG2NBpgbBVfXjl1GagTni4RZHxP&amp;protocol=oauth2&amp;redirect_uri=https:%2F%2Fpower.dat.com%2Fsignin-auth0&amp;response_mode=form_post&amp;response_type=code%20id_token%20token&amp;scope=openid%20profile%20offline_access%20email&amp;nonce=638132235803550326.MmM3ZTVmZjktZTc1MS00Zjg3LWI3ZTQtYzI3MDZkZDlkNjQ1ZDJlYTU5NmYtM2IxYi00MWI0LTkyYWEtN2Q2Njg3Y2JkZDFm&amp;app_name=Power&amp;page_mode=Legacy&amp;audience=https:%2F%2Fprod-api.dat.com&amp;x-client-SKU=ID_NET451&amp;x-client-ver=5.2.1.0&amp;capturedTime=1677626781197" target="_blank">
                                                         <button class="btn btn-outline-secondary w-100 btn-sm text-nowrap mb-2" type="button">
                                                             <i class="fas fa-share-square"></i>&nbsp;Open
                                                         </button>
