6fc5a6ebafa6411bf0097ce311f789983e789b80
rjrsand <99208059+rjrsand@users.noreply.github.com>
2023-03-07 20:15:37 -0500

    started directory.php

---
+++ directory.php
@@ -0,0 +1,87 @@
+<!doctype html>
+<html>
+<head>
+<base href="/">
+    
+<script src="dmxAppConnect/dmxAppConnect.js"></script>
+<meta charset="UTF-8">
+<title>Untitled Document</title>
+
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css" integrity="sha384-HzLeBuhoNPvSl5KYnjx0BT+WB0QEEqLprO+NBkkk5gbc67FTaL7XIGa2w1L0Xbgc" crossorigin="anonymous" />
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link rel="stylesheet" href="bootstrap/5/css/bootstrap.min.css" />
+<link rel="stylesheet" href="css/style.css" />
+</head>
+<body is="dmx-app" id="directory">
+
+
+<header></header>
+<main class="mt-5 mb-5">
+<div class="container">
+<div class="row min-vh-75"><div class="col">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <h5 class="text-warning text-uppercase">The Pack</h5>
+                    <h1 class="text-left fw-bold text-light">Directory</h1>
+                    <p class="mb-4 text-secondary">Here you can find the contact information for everyone at RWS.</p>
+                    </div>
+                </div>
+                <div class="row">
+                <div class="col">
+<div class="d-flex">
+<div class="card-deck w-25">
+<div class="card">
+  <div class="card-header">Operations</div>
+  <img class="card-img-top" alt="Card image cap" src="assets/images/Intranet/wolf-pack-png-image-wolf-pack-transparent-background-11562897883xolw6cjtyo_low.png">
+  <div class="card-body">
+    <h4 class="card-title">Card title</h4>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+   <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+</div>
+<div class="card-deck w-25">
+<div class="card">
+  <div class="card-header">Sales</div>
+  <img class="card-img-top" alt="Card image cap" src="assets/images/Intranet/62-621375_best-seller-best-selling-logo-png.png">
+  <div class="card-body">
+    <h4 class="card-title">Card title</h4>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+   <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+</div>
+<div class="card-deck w-25">
+<div class="card">
+  <div class="card-header">Accounting</div>
+  <img class="card-img-top" alt="Card image cap" src="assets/images/Intranet/0223.png_300.png">
+  <div class="card-body">
+    <h4 class="card-title">Card title</h4>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+   <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+</div>
+<div class="card-deck w-25">
+<div class="card">
+  <div class="card-header">I.T.</div>
+  <img class="card-img-top" alt="Card image cap" src="assets/images/Intranet/30-301075_the-it-crowd-mpi-home-video-it-crowd.png">
+  <div class="card-body">
+    <h4 class="card-title">Card title</h4>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+   <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+</div>
+</div>
+                    </div>
+                </div>
+            </div>
+        </div></div>
+    
+</div> </main>
+<footer></footer>
+<script src="bootstrap/5/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
