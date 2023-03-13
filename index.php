916078e0217db4ce3daa92ba33e6a40227dbec8e
rjrsand <99208059+rjrsand@users.noreply.github.com>
2023-03-13 16:11:16 -0400

    made tiles clickable

--- index.php
+++ index.php
@@ -46,7 +46,7 @@
                         </div>
                         <div class="row">
                             <div class="col-lg-6 align-self-center">
-                                <div class="d-flex tile-category mb-3 pt-3 pb-3 ps-2 pe-2 align-items-center h-tile-default">
+                                <div class="d-flex tile-category mb-3 pt-3 pb-3 ps-2 pe-2 align-items-center h-tile-default tile-hover">
                                     <div class="d-flex align-items-center w-100p justify-content-center">
                                         <h1 class="mb-0 text-white-50"><i class="fas fa-link"></i></h1>
                                     </div>
@@ -58,7 +58,7 @@
 
                             </div>
                             <div class="col-lg-6">
-                                <div class="d-flex tile-category mb-3 pt-3 pb-3 ps-2 pe-2 align-items-center h-tile-default">
+                                <div class="d-flex tile-category mb-3 pt-3 pb-3 ps-2 pe-2 align-items-center h-tile-default tile-hover" dmx-on:click="browser1.goto('document.php')">
                                     <div class="d-flex align-items-center w-100p justify-content-center">
                                         <h1 class="mb-0 text-white-50"><i class="far fa-file-alt"></i></h1>
                                     </div>
