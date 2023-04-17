b944e56155d5863b825a851b65842f1abfeaa13f
rjrsand <99208059+rjrsand@users.noreply.github.com>
2023-04-17 12:57:00 -0400

    test

--- index.php
+++ index.php
@@ -209,7 +209,7 @@
                                 </div>
                             </div>
                             <div class="col-lg-6">
-                                <div class="d-flex tile-category mb-3 pt-3 pb-3 ps-2 pe-2 align-items-center h-tile-default tile-hover" dmx-on:click="">
+                                <div class="d-flex tile-category mb-3 pt-3 pb-3 ps-2 pe-2 align-items-center h-tile-default tile-hover" dmx-on:click="browser1.goto('unauthorized.php')">
                                     <div class="d-flex align-items-center w-100p justify-content-center">
                                         <h1 class="mb-0 text-white-50"><i class="fas fa-thumbs-up"></i></h1>
                                     </div>
