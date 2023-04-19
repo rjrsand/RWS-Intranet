672301e1cbd55ac5b2853d657341678738d20ba5
rjrsand <99208059+rjrsand@users.noreply.github.com>
2023-04-19 16:03:53 -0400

    corrected file type sop

--- s-o-p.php
+++ s-o-p.php
@@ -95,7 +95,7 @@
                         </div>
                         <div class="row">
                             <div class="col-lg-6 align-self-center">
-                                <div class="d-flex tile-category mb-3 pt-3 pb-3 ps-2 pe-2 align-items-center h-tile-default tile-hover" dmx-on:click="browser1.goto('assets/Docs/Shipping-Order-Tutorial.dotx')">
+                                <div class="d-flex tile-category mb-3 pt-3 pb-3 ps-2 pe-2 align-items-center h-tile-default tile-hover" dmx-on:click="browser1.goto('assets/Docs/Shipping-Order-Tutorial.pdf')">
                                     <div class="d-flex align-items-center w-100p justify-content-center">
                                         <h1 class="mb-0 text-white-50"><i class="fab fa-salesforce"></i></h1>
                                     </div>
