aa11aa42dd7f93304c4e689d10822b8643a1aa64
rjrsand <99208059+rjrsand@users.noreply.github.com>
2024-07-10 15:46:24 -0400

    test

--- admin.php
+++ admin.php
@@ -18,7 +18,7 @@
     <link rel="stylesheet" href="css/stylesheet.css" />
     <link rel="stylesheet" href="css/intranet.css" />
     <script src="bootstrap/5/js/bootstrap.bundle.min.js"></script>
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha384-ZvpUoO/+PpLXR1lu4jmpXWu80pZlYUAfxl5NsBMWOEPSjUn/6Z/hRTt8+pR6L4N2" crossorigin="anonymous"></script>
     <script src="dmxAppConnect/dmxBootstrap5Navigation/dmxBootstrap5Navigation.js" defer></script>
 
     <script src="dmxAppConnect/dmxBrowser/dmxBrowser.js" defer></script>
