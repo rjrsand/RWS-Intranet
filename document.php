9cd363a06096565a515bb688ceddd8888a6b44a4
rjrsand <99208059+rjrsand@users.noreply.github.com>
2024-12-06 16:11:12 -0500

    adjusting text settings

--- document.php
+++ document.php
@@ -50,7 +50,10 @@
     <div class="modal" id="PTO" is="dmx-bs5-modal" tabindex="-1">
         <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-xl" role="document">
             <div class="modal-content">
-                <iframe src="https://usebasin.com/form/b261aef3c8a3/view/54a55660cca9?iframe=true" frameborder="0" style="border: none; overflow: hidden;" onload="var self = this; self.contentWindow.postMessage('getHeight', '*'); setInterval(function() { self.contentWindow.postMessage('getHeight', '*'); }, 500); window.addEventListener('message', function(event) { if (event.data.action === 'setHeight') { self.style.height = event.data.height + 'px'; } });" width="100%"></iframe>
+                <iframe class="basinIframe" src="https://usebasin.com/form/6d6d0bbfdc4a/view/938d4a04c1d7?iframe=true" allowtransparency="true" frameborder="0" style="border: none; overflow: hidden;" onload="var self = this; self.contentWindow.postMessage('getHeight', '*'); setInterval(function() { self.contentWindow.postMessage('getHeight', '*'); }, 500); window.addEventListener('message', function(event) { if (event.data.action === 'setHeight') { self.style.height = event.data.height + 'px'; } });" width="100%"></iframe>
+                <script type="text/javascript">
+                    window.addEventListener('message', function(event) { if (event.data.action === 'redirect') { window.location.href = event.data.url; } }); 
+                </script>
             </div>
         </div>
     </div>
