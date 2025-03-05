81cd7a3200b1ffac3396fb360ef114f58916cc3d
rjrsand <99208059+rjrsand@users.noreply.github.com>
2025-03-05 13:52:13 -0500

    flashing fix 2

--- document.php
+++ document.php
@@ -50,7 +50,7 @@
     <div class="modal" id="PTO" is="dmx-bs5-modal" tabindex="-1">
         <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-xl" role="document">
             <div class="modal-content">
-                <iframe class="basinIframe" src="https://usebasin.com/form/5ab4c4f9c2ff/view/383db0d2837c?iframe=true" frameborder="0" style="border: none; overflow: hidden;" onload="var self = this; self.contentWindow.postMessage('getHeight', '*'); setInterval(function() { self.contentWindow.postMessage('getHeight', '*'); }, 500); window.addEventListener('message', function(event) { if (event.data.action === 'setHeight') { self.style.height = event.data.height + 'px'; } });" width="100%"></iframe>
+                <iframe class="basinIframe" src="https://usebasin.com/form/5ab4c4f9c2ff/view/383db0d2837c?iframe=true" frameborder="0" style="border: none; overflow: hidden; height: 800px;" width="100%"></iframe>
                 <script type="text/javascript">
                     window.addEventListener('message', function(event) { if (event.data.action === 'redirect') { window.location.href = event.data.url; } }); 
                 </script>
