4dbaaf859f15057ec271a006f81ecb709c959ff8
rjrsand <99208059+rjrsand@users.noreply.github.com>
2025-03-05 14:01:16 -0500

    test

--- document.php
+++ document.php
@@ -60,7 +60,7 @@
     <div class="modal" id="OT" is="dmx-bs5-modal" tabindex="-1">
         <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-xl" role="document">
             <div class="modal-content">
-                <iframe class="basinIframe" src="https://usebasin.com/form/6d6d0bbfdc4a/view/938d4a04c1d7?iframe=true" allowtransparency="true" frameborder="0" style="border: none; overflow: hidden;" onload="var self = this; self.contentWindow.postMessage('getHeight', '*'); setInterval(function() { self.contentWindow.postMessage('getHeight', '*'); }, 500); window.addEventListener('message', function(event) { if (event.data.action === 'setHeight') { self.style.height = event.data.height + 'px'; } });" width="100%"></iframe>
+                <iframe class="basinIframe" src="https://usebasin.com/form/6d6d0bbfdc4a/view/938d4a04c1d7?iframe=true" allowtransparency="true" frameborder="0" style="border: none; overflow: hidden; height: 800px" width="100%"></iframe>
                 <script type="text/javascript">
                     window.addEventListener('message', function(event) { if (event.data.action === 'redirect') { window.location.href = event.data.url; } }); 
                 </script>
